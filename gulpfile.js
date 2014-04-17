e67e11a31cae44850da78d7608ea496c61b3acf4
Kevin Gustavson <kdgustavson@gmail.com>
2014-04-17 10:48:38 -0500

    Return streams for synchronous tasks where possible.

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ var clean = require('gulp-clean');
 var concat = require('gulp-concat');
 var coveralls = require('gulp-coveralls');
 var exec = require('gulp-exec');
+var eventStream = require('event-stream');
 var jshint = require('gulp-jshint');
 var karma = require('karma').server;
 var gulpkarma = require('gulp-karma');
 var minifyCSS = require('gulp-minify-css');
+var mocha = require('gulp-mocha');
 var notify = require('gulp-notify');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
@@ -15,41 +17,28 @@ var gutil = require('gulp-util');
 var sources = ['messi.js', 'test/*.js'];
 var testfiles = [
     'jquery.min.js',
-    'http://chaijs.com/chai.js',
-    'http://cdnjs.cloudflare.com/ajax/lib/mocha/1.13.0/mocha.min.js',
+    //'http://chaijs.com/chai.js',
+    //'http://cdnjs.cloudflare.com/ajax/lib/mocha/1.13.0/mocha.min.js',
     'messi.js',
     'extensions/**/*.js',
     'test/**/*.js'
 ];
 
-gulp.task('clean', function(done) {
+gulp.task('clean', function() {
     gulp.src([ './messi.min.js', './messi.min.js.map', './messi.min.css' ])
         .pipe(clean());
-
-    done();
 });
 
-gulp.task('lint', function(done) {
-    gulp.src(sources)
+gulp.task('lint', function() {
+    return gulp.src(sources)
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
-    done();
 });
 
 gulp.task('test', ['combine'], function(done) {
     return karma.start(
         {
-            browsers: ['PhantomWithConfig'],
-            customLaunchers: {
-                PhantomWithConfig: {
-                    base: 'PhantomJS',
-                    options: {
-                        page: {
-                            viewportSize: { width: 640, height: 480 }
-                        }
-                    }
-                }
-            },
+            browsers: ['PhantomJS'],
             files: [
                 'http://cdnjs.cloudflare.com/ajax/lib/mocha/1.13.0/mocha.min.js',
                 'http://chaijs.com/chai.js',
@@ -73,63 +62,57 @@ gulp.task('test', ['combine'], function(done) {
         });
 });
 
-gulp.task('test2', ['combine'], function(done) {
-    gulp.src(testfiles)
+gulp.task('test2', ['combine'], function() {
+    return gulp.src(testfiles)
         .pipe(gulpkarma({
             configFile: 'karma.conf.js',
             action: 'run'
         }))
         .on('error', function(err) {
             throw err; // Make sure failed tests cause gulp to exit non-zero
-        })
-        .on('end', done);
+        });
 });
 
-gulp.task('combine', ['clean'], function(done) {
-    gulp.src(['messi.js', 'extensions/*.js'])
+gulp.task('combine', ['clean'], function() {
+    return gulp.src(['messi.js', 'extensions/*.js'])
         .pipe(concat('messi-full.js'))
         .pipe(gulp.dest('./'));
-    done();
 });
 
-gulp.task('compress', ['combine'], function(done) {
-    gulp.src('messi.js')
-        .pipe(rename('messi.min.js'))
-        .pipe(uglify({outSourceMap: true}))
-        .pipe(gulp.dest('./'));
+gulp.task('compress', ['combine'], function() {
+    return eventStream.merge(
+        gulp.src('messi.js')
+            .pipe(rename('messi.min.js'))
+            .pipe(uglify({outSourceMap: true}))
+            .pipe(gulp.dest('./')),
 
-    gulp.src('messi-full.js')
-        .pipe(rename('messi-full.min.js'))
-        .pipe(uglify({outSourceMap: true}))
-        .pipe(gulp.dest('./'));
+        gulp.src('messi-full.js')
+            .pipe(rename('messi-full.min.js'))
+            .pipe(uglify({outSourceMap: true}))
+            .pipe(gulp.dest('./')),
 
-    gulp.src('messi.css')
-        .pipe(rename('messi.min.css'))
-        .pipe(minifyCSS())
-        .pipe(gulp.dest('./'));
-
-    done();
+        gulp.src('messi.css')
+            .pipe(rename('messi.min.css'))
+            .pipe(minifyCSS())
+            .pipe(gulp.dest('./'))
+    );
 });
 
-gulp.task('codecoverage', ['test2'], function(done) {
-    setTimeout(function() {
-        gulp.src('coverage/**/lcov.info')
-            .pipe(coveralls());
-        done();
-    }, 3000);
+gulp.task('codecoverage', ['test'], function() {
+    return gulp.src('coverage/**/lcov.info')
+        .pipe(coveralls());
 });
 
 gulp.task('watch', function() {
-    gulp.watch(sources, ['default']);
+    return gulp.watch(sources, ['default']);
 });
 
-gulp.task('notify', function(done) {
-    gulp.src('./gulpfile.js')
+gulp.task('notify', ['test'], function() {
+    return gulp.src('./gulpfile.js')
         .pipe(notify({ message: 'All done, master!' }));
-    done();
 });
 
-gulp.task('default', ['lint', 'compress', 'test2', 'notify']);
+gulp.task('default', ['lint', 'compress', 'test', 'notify']);
 
-gulp.task('travis-test', ['lint', 'compress', 'test2', 'codecoverage']);
+gulp.task('travis-test', ['lint', 'compress', 'test', 'codecoverage']);
 
